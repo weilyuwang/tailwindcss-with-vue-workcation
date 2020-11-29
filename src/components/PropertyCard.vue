093e9f3175b226c6a8e0e24df136a36e5de73645
weilyuwang <weilyuwang@gmail.com>
2020-11-28 21:40:16 -0500

    :fire: scaffold card compoennt

---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white p-6">
+    <img :src="property.imageUrl" :alt="property.imageAlt" />
+    <h4>{{ property.title }}</h4>
+    <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+    <div>{{ property.formattedPrice }} / week</div>
+    <div>
+      {{ property.rating }} / 5 stars (based on
+      {{ property.reviewCount }} reviews)
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "PropertyCard",
+  props: ["property"],
+};
+</script>
