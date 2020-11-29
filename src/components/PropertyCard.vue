58f069e368cdf166584e730c9d5cfba71f5d6108
weilyuwang <weilyuwang@gmail.com>
2020-11-28 21:58:08 -0500

    :fire: basic card structure

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="bg-white p-6">
+  <div class="bg-white rounded-lg overflow-hidden">
     <img :src="property.imageUrl" :alt="property.imageAlt" />
-    <h4>{{ property.title }}</h4>
-    <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
-    <div>{{ property.formattedPrice }} / week</div>
-    <div>
-      {{ property.rating }} / 5 stars (based on
-      {{ property.reviewCount }} reviews)
+
+    <div class="p-6">
+      <h4>{{ property.title }}</h4>
+      <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+      <div>{{ property.formattedPrice }} / week</div>
+      <div>
+        {{ property.rating }} / 5 stars (based on
+        {{ property.reviewCount }} reviews)
+      </div>
     </div>
   </div>
 </template>
