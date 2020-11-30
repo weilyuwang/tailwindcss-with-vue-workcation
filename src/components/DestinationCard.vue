603ce79f6800fad560dc838135dd9152d2242118
weilyuwang <weilyuwang@gmail.com>
2020-11-30 16:52:42 -0500

    :sparkles: beautify destination card on iphonex

--- src/components/DestinationCard.vue
+++ src/components/DestinationCard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex items-center rounded-lg bg-white shadow-lg overflow-hidden">
+  <div
+    class="flex xs:items-center rounded-lg bg-white shadow-lg overflow-hidden"
+  >
     <!-- flex-shrink-0: The element will not shrink: it will retain the width it needs, and not wrap its content. 
     Its siblings will shrink to give space to the target element. -->
     <img
@@ -7,13 +9,17 @@
       :src="destination.imageUrl"
       :alt="destination.imageAlt"
     />
-    <div class="px-4 py-2 xs:px-6 xs:py-4">
-      <h3 class="text-md xs:text-lg font-semibold text-gray-800">
-        {{ destination.city }}
-      </h3>
-      <p class="text-gray-600 text-xs xs:text-base">
-        ${{ destination.averagePrice }} / night average
-      </p>
+    <div
+      class="px-4 xs:px-6 py-4 flex flex-col items-start justify-between xs:block"
+    >
+      <div>
+        <h3 class="text-md xs:text-lg font-semibold text-gray-800">
+          {{ destination.city }}
+        </h3>
+        <p class="text-gray-600 mt-2 xs:mt-0 text-sm xs:text-base">
+          ${{ destination.averagePrice }} / night average
+        </p>
+      </div>
       <div class="mt-2 xs:mt-4 leading-none">
         <a href="#" class="text-brand-blue font-semibold text-xs xs:text-sm"
           >Explore {{ destination.propertyCount }} properties</a
